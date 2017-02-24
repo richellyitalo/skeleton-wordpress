fbc5b2bdd328faec2d3be7ac8b47ca526e5e7515
Richelly Italo <Richelly Italo>
2017-02-24 11:27:32 -0300

    Meta mobile.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.png" />
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.ico" type="image/x-icon">
